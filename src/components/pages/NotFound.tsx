e051333fd36b0adf6bf3acf6ed710c5add6b6bca
calixtollucas <lucaslucenacalixto1@gmail.com>
2025-07-30 22:30:06 -0300

    initial commit

---
+++ src/components/pages/NotFound.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+    return (
+        <div className="not-found">
+            <h1>404 - Page Not Found</h1>
+            <p>Sorry, the page you are looking for does not exist.</p>
+            <p>Please check the URL or return to the home page.</p>
+            <a href="/">Go to Home</a>
+        </div>)
+}
