c846c6b8e67d067e57964edaea75acaeea529cf9
calixtollucas <lucaslucenacalixto1@gmail.com>
2025-08-05 13:26:22 -0300

    fix: image path

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -36,7 +36,7 @@ export default function Hero() {
                     }
                 }
             >
-                <img src="../../../public/euwebp.webp"
+                <img src="/euwebp.webp"
                     alt="Hero Image"
                     className="hero-image rounded-full w-64 h-64 object-cover shadow-lg"
                 />
